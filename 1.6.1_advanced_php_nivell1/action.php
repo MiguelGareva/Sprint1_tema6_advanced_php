c64e785f9f8200f3609d9d1546e02cfdc98e69ca
miguel garcia <miguel.gareva@gmail.com>
2024-12-10 11:29:53 +0100

    cambios en form.html y logicas en action.php implementadas

--- 1.6.1_advanced_php_nivell1/action.php
+++ 1.6.1_advanced_php_nivell1/action.php
@@ -1,1 +1,10 @@
-<?php ?>
+<?php 
+
+$_SESSION["name"] = htmlspecialchars($_POST["name"]);
+
+echo "Nombre: " . $_SESSION["name"] . "<br>";
+echo "Apellido: " . htmlspecialchars($_POST["fname"]) . "<br>";
+echo "Color favorito: " . htmlspecialchars($_POST["favcolor"]) . "<br>";
+echo "Su mascota se llama: " . htmlspecialchars($_POST["petname"]) . "<br>";
+
+?>
