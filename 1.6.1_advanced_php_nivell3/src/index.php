2787a07a2a1eca20bdb6ee661ba338020c47bef5
miguel garcia <miguel.gareva@gmail.com>
2025-01-16 14:38:22 +0100

    program using fakerphp/faker package created on index.php

--- 1.6.1_advanced_php_nivell3/src/index.php
+++ 1.6.1_advanced_php_nivell3/src/index.php
@@ -1,4 +1,11 @@
 <?php 
-require_once "vendor/autoload.php";
+require_once "../vendor/autoload.php";
+
+$faker = Faker\Factory::create();
+
+echo "Nombre: " . $faker->name . "<br>";
+echo "Dirección: " . $faker->address . "<br>";
+echo "Nº teléfono: " . $faker->phoneNumber . "<br>";
+echo "E-mail: " . $faker->email . "<br>";
 
 ?>
